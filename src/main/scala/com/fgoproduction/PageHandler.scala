af91750b9aacd57221f1823ac8bbc4f76b842d64
fgo <fgoinai@gmail.com>
2018-12-19 00:12:00 +0800

    remove deprecated scala.collection.JavaConversions._ dependency

--- src/main/scala/com/fgoproduction/PageHandler.scala
+++ src/main/scala/com/fgoproduction/PageHandler.scala
@@ -5,7 +5,7 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 import scala.io.Source.fromURL
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 
 sealed class PageHandler(url: String) {
@@ -14,10 +14,10 @@ sealed class PageHandler(url: String) {
   def iter(): Iterator[String] = src split '\n' iterator
   def doc(): Document = Jsoup parse src
   def selectDoc(selector: String): Elements = doc select selector
-  def getHrefs(selector: String): List[String] = selectDoc(selector).toList.map(_ attr "href")
+  def getHrefs(selector: String): List[String] = selectDoc(selector).asScala.map(_ attr "href").toList
 }
 
 class CategoryPageHandler(url: String) extends PageHandler(url) {
-  
+
 }
 

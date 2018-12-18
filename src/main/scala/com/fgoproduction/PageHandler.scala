837480c0ab7f7c21571ee6d07b5eecbb0fe5d005
fgo <fgoinai@gmail.com>
2018-12-19 00:06:59 +0800

    update some method

--- src/main/scala/com/fgoproduction/PageHandler.scala
+++ src/main/scala/com/fgoproduction/PageHandler.scala
@@ -2,14 +2,22 @@ package com.fgoproduction
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 import scala.io.Source.fromURL
+import scala.collection.JavaConversions._
 
-class PageHandler(url: String) {
-  private val src: String = fromURL(url).mkString
 
-  def iter(): Iterator[String] = this.src split '\n' iterator
+sealed class PageHandler(url: String) {
+  protected val src: String = fromURL(url).mkString
 
+  def iter(): Iterator[String] = src split '\n' iterator
   def doc(): Document = Jsoup parse src
+  def selectDoc(selector: String): Elements = doc select selector
+  def getHrefs(selector: String): List[String] = selectDoc(selector).toList.map(_ attr "href")
+}
+
+class CategoryPageHandler(url: String) extends PageHandler(url) {
+  
 }
 

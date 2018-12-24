b3a6dcc0208f2457056d287a9754047cffe9098b
Phil Lui <phillui@atal.com.hk>
2018-12-24 15:34:29 +0800

    implemented dlgoo and dlimage

--- src/main/scala/com/fgoproduction/Main.scala
+++ src/main/scala/com/fgoproduction/Main.scala
@@ -7,6 +7,8 @@ import scala.language.postfixOps
 
 
 object Main extends App {
+  implicit val pool: ExecutorService = java.util.concurrent.Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors)
+
   def initDB(): Unit = {
     List(new Series(), new RawBookDetail(), new Book(), new Tag(), new TagBookMap())
       .foreach(_ ())
@@ -14,16 +16,20 @@ object Main extends App {
   override def main(args: Array[String]): Unit = {
     initDB()
     val url = "http://epubln.blogspot.com/"
-    val pool = java.util.concurrent.Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors)
-    exec(pool, new CategoryPageHandler(url)())
-    //    println(new DownloadPageHandler("http://epubln.blogspot.com/2018/11/06_7.html").getImgLink)
+    //    val t = LocalTime.now()
+    //    exec(new CategoryPageHandler(url)())
+    //    println(Duration.between(t, LocalTime.now()).toSeconds)
+
+    new DownloadImageHandler("https://3.bp.blogspot.com/-Pi4U_CUuokc/XAvLN6hdqdI/AAAAAAAABOg/-ORomqm4RWw2EiHLPuGf_U65UHpj4NraACLcBGAs/s1600/16.jpg")()
+    new DownloadGooHandler("https://drive.google.com/open?id=1dah5YX6AE62c4v88MoAq2OUkHkg2XIJa")()
+    new DownloadGooHandler("https://drive.google.com/file/d/18gS4yWC83a6KSNVy0jdijL5bqAEopuDE/view?usp=sharing")()
   }
 
-  @tailrec def exec(pool: ExecutorService, it: List[PageHandler]): List[PageHandler] = {
+  @tailrec def exec(it: List[PageHandler])(implicit pool: ExecutorService): List[PageHandler] = {
     if (it.isEmpty) {
       pool.shutdownNow()
       return List()
     }
-    exec(pool, it.map(x => pool.submit(() => x())).flatMap(_ get))
+    exec(it.tail ++ it.head())
   }
 }
--- src/main/scala/com/fgoproduction/PageHandler.scala
+++ src/main/scala/com/fgoproduction/PageHandler.scala
@@ -1,7 +1,11 @@
 package com.fgoproduction
 
+import java.io.{File, FileOutputStream}
+import java.net.{URL, URLDecoder}
+import java.nio.channels.Channels
 import java.util.NoSuchElementException
 
+import javax.net.ssl.HttpsURLConnection
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
@@ -27,6 +31,23 @@ sealed trait PageHandler {
 
   def selectDoc(url: String, selector: String): Elements =
     doc select selector
+
+  def downloadImage(url: String, path: String): Unit = {
+    val link = new URL(url)
+    val conn = link.openConnection().asInstanceOf[HttpsURLConnection]
+    val fieldValue = conn.getHeaderField("Content-Disposition")
+
+    val fileName = URLDecoder.decode(if (fieldValue.contains("filename*=UTF-8''")) {
+      fieldValue.substring(fieldValue.indexOf("filename*=UTF-8''") + 17, fieldValue.length())
+    } else {
+      fieldValue.substring(fieldValue.indexOf("filename=\"") + 10, fieldValue.length() - 1)
+    }, "UTF-8")
+    val ch = Channels.newChannel(link.openStream())
+    val fos = new FileOutputStream(s"$path${File.separator}$fileName")
+    fos.getChannel.transferFrom(ch, 0, Long.MaxValue)
+    fos.close()
+  }
+
 }
 
 class CategoryPageHandler(url: String) extends PageHandler {
@@ -93,34 +114,60 @@ class DownloadPageHandler(url: String) extends PageHandler {
       .map(_ attr "href")
       .find(
         x =>
-          x.contains("drive.google") || x.contains("mega") || x.contains(
-            "adf.ly") || x.contains("docs.google") || x.contains("mediafire"))
+          x.contains("drive.google") ||
+            x.contains("mega") ||
+            x.contains("adf.ly") ||
+            x.contains("docs.google") ||
+            x.contains("mediafire")
+      )
 }
 
 class DownloadImageHandler(url: String) extends PageHandler {
   override lazy val doc: Document = docFn(url)
 
   override def apply(): List[PageHandler] = {
+    // TODO get path from db, then save to path
+    val path = System.getProperty("user.dir")
+    downloadImage(url, path)
     List()
-
   }
 }
 
 class DownloadGooHandler(url: String) extends PageHandler {
   override lazy val doc: Document = docFn(url)
 
-  override def apply(): List[PageHandler] = {
-    List()
+  val dlLink: String = {
+    val prefix = if (url.contains("drive.google.com")) {
+      "drive"
+    } else {
+      "docs"
+    }
+    val dlPrefix: String = s"https://$prefix.google.com/uc?export=download&id="
+    List("open?id=", "file/d/")
+      .map(x => s"https://$prefix.google.com/$x")
+      .filter(url startsWith)
+      .map(x => s"$dlPrefix${url.replace(x, "").split('/').head}")
+      .head
+  }
 
+  override def apply(): List[PageHandler] = {
+    // TODO
+    downloadImage(dlLink, System.getProperty("user.dir"))
+    List()
   }
 }
 
 class DownloadMegaHandler(url: String) extends PageHandler {
   override lazy val doc: Document = docFn(url)
+  val dlPrefix = "https://drive.google.com/uc?export=download&id="
+
+  //  def convertDownloadLink(link: String): String = {
+  //    val drivePrefix = """(\w+?).google.com""".r.findAllIn(link).matchData.next()
+  //  }
 
   override def apply(): List[PageHandler] = {
+    // TODO selenium
     List()
-
   }
 }
 
@@ -129,6 +176,5 @@ class AdflyHandler(url: String) extends PageHandler {
 
   override def apply(): List[PageHandler] = {
     List()
-
   }
 }
